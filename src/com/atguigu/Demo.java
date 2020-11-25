b9b1aa245625b5a4fea6e86985cfdb95fcc137ea
SongKK <KuokuanSong@163.com>
2020-11-25 21:11:06 +0800

    test

---
+++ src/com/atguigu/Demo.java
@@ -0,0 +1,15 @@
+package com.atguigu;
+
+import java.util.Random;
+
+/**
+ * @author Kuokuan Song
+ * @date 2020/11/25
+ */
+public class Demo {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println(new Random().toString());
+        }
+    }
+}
